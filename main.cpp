9e779dad151b089a699597c28a4546fa56ac3bfa
Kaya-Sem <kayasemvc@gmail.com>
2024-11-14 13:15:49 +0100

    watch env unix user

--- main.cpp
+++ main.cpp
@@ -3,15 +3,18 @@
 #include "watcher.hpp"
 #include <atomic>
 #include <iostream>
+#include <ostream>
 #include <thread>
 
 using namespace std;
 using namespace wtr;
 
 /* 1) Event is processed from the queue. This is passed to the relevant
- *    transducer. Transducers are (usually) based on file extension. 
- * 2) the transducer gives back a key-value map, that indicate labels and relations. 
- * 3) These values are passed to the appropriate file, it's metadata being updated. 
+ *    transducer. Transducers are (usually) based on file extension.
+ * 2) the transducer gives back a key-value map, that indicate labels and
+ * relations. 
+ * 3) These values are passed to the appropriate file, it's metadata
+ * being updated. 
  * 4) The node in the graph/database gets updated
  */
 
@@ -33,12 +36,18 @@ void processQueue() {
 }
 
 int main() {
+  // Get the user's home directory from the environment variable
+  const char* homeDir = std::getenv("HOME");
+  if (!homeDir) {
+    cerr << "Failed to get home directory" << endl;
+    return 1;
+  }
+
   // Start the event processing thread
   thread processingThread(processQueue);
 
-  // Watch the current directory asynchronously, calling the provided function
-  // on each event
-  auto watcher = watch("/home/kayasem", queueEvent);
+  // Watch the user's home directory asynchronously
+  auto watcher = watch(homeDir, queueEvent);
 
   // Main thread can continue doing other work here
   // (We will just wait for a newline to terminate the program)
