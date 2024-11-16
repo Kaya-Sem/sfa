11ddca2043175745bfd62afb83d0cb5d0809d4b8
Kaya-Sem <kayasemvc@gmail.com>
2024-11-16 16:24:23 +0100

    added documentation and basics for triplestore

--- transducer/Transducer.hpp
+++ transducer/Transducer.hpp
@@ -1,10 +1,29 @@
 #ifndef TRANSDUCER_HPP
 #define TRANSDUCER_HPP
 
-#include <map>
+#include <set>
 #include <string>
 
 using namespace std;
+
+/* A transducer for a file gives back triples. These triples
+ * are used for the Triplestore.
+ * See https://en.wikipedia.org/wiki/Triplestore
+ * A triple is a data entity composed of subject-predicate-object, like "Bob is
+ * 35"
+ */
+
+enum Predicate { hasTitle, hasSize };
+
+//  TODO: make subject and object classes / enums of their own
+class Triple {
+public:
+  string subject;
+  Predicate predicate;
+  string object;
+  Triple(string subject, Predicate predicate, string object)
+      : subject(subject), predicate(predicate), object(object) {}
+};
 
 class File {
 public:
@@ -12,10 +31,13 @@ public:
   string path;
 };
 
+/* A transducer takes in a file, and gives back a collection of Triple objects.
+ * These are to be used in the semantic graph and triplestore
+ */
 class Transducer {
 public:
   virtual ~Transducer() = default;
-  virtual map<string, string> process(const File &file) const = 0;
+  virtual set<Triple> process(const File &file) const = 0;
 };
 
 #endif
